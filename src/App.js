c59c81e499463a2ca4d4958a0eaf9557356ceab3
taltla1028 <rainyw1028@naver.com>
2021-05-22 14:36:55 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import Main from './pages/Main';
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
